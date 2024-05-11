843391632174a304496274f2908314cab92bcdc1
exe <you@example.com>
2024-05-10 23:54:18 -0300

    ejercicios CSS

---
+++ PHP/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php echo "hola"; ?>
+</body>
+
+</html>
+
+<!-- include y requiere
+    tipos escalares,compuestos,especiales
+    debilmente tipado
+ 
+    tipo String
+        '': admite caracter de escape, no te muestra variables se tiene que concatenar
+        "": es posible mostrar los valores de la variables
+    
+    variables predefinidas
+
+    constantes
+
+        define, no lleva el signo $
+    
+    @ operador de control de error 
+
+    estructuras selectivas
+
+    estructuras repetivas
+
+    #2 Ejercicio imprimir la tabla de multiplicar para el viernes
+
+
+    parametro por valor y por referencia
+
+    tablas o diccionarios
+
+    foreach
+
+    bibliotecas de funciones
+
+        cadena,fechas,arrays,
+    #3 fecha actual
+
+
+    registrer global = off, donde se utiliza $_POST para recepcionar los valores
+     o $_REQUEST
+    httpconf configuracion del servidor
+    php.ini configuracion de php
+
+    los valores de hidden se ven en el html consola de desarrollo
+    
+
+    -->
